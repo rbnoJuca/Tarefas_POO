b2945be68c5b04604dbe2cf0a8460da68148b3cd
rbnoJuca <rodolfo.bergamo@gmail.com>
2020-09-16 15:22:21 -0300

    Aula 31-08

---
+++ Aula 31-08/ConsoleApp/Produto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Produto
+    {
+        private string nome;
+        public string Nome { get => nome; set => nome = value; }
+
+        private double preco;
+        public double Preco { get => preco; set => preco = value; }
+
+        private int quantidade;
+        public int Quantidade 
+        { 
+            get => quantidade;
+            set
+            {
+                quantidade = value;
+                Total = quantidade * Preco;
+            }
+        }
+
+        private double total;
+        public double Total { get => total; set => total = value; }
+
+        public string ExibirProduto()
+        {
+            string v_sProduto = "Dados do Produto: " + Nome + ", R$ " + Preco.ToString() + ", " + Quantidade.ToString() + " unidades, Total: R$" + Total.ToString();
+            return v_sProduto;
+        }
+
+        public void Adicionar(int p_iQtd)
+        {
+            Quantidade = Quantidade + p_iQtd;
+        }
+
+        public void Remover(int p_iQtd)
+        {
+            Quantidade = Quantidade - p_iQtd;
+        }
+
+    }
+}
---
+++ Aula 31-08/ConsoleApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto();
+
+            string v_sOpcao = "";
+            do
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("");
+                Console.WriteLine("1 - Cadastrar Produto");
+                Console.WriteLine("");
+                Console.WriteLine("2 - Adicionar Produto");
+                Console.WriteLine("");
+                Console.WriteLine("3 - Remover Produto");
+                Console.WriteLine("");
+                Console.WriteLine("5 - Sair");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("Lista de Produto");
+                Console.WriteLine(produto.ExibirProduto());
+                v_sOpcao = Console.ReadLine();
+
+                switch(v_sOpcao)
+                {
+                    case "1":
+                        Console.WriteLine("Informe o nome do produto: ");
+                        produto.Nome = Console.ReadLine();
+                        Console.WriteLine("Informe o preço do produto: ");
+                        produto.Preco = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Informe a quantidade de produto: ");
+                        produto.Quantidade = int.Parse(Console.ReadLine());
+                        break;
+                    case "2":
+                        Console.WriteLine("Informe a quantidade de produto para adiconar: ");
+                        produto.Adicionar(int.Parse(Console.ReadLine())) ;
+                        break;
+                    case "3":
+                        Console.WriteLine("Informe a quantidade de produto para remover: ");
+                        produto.Remover(int.Parse(Console.ReadLine()));
+                        break;
+                }
+            } while (v_sOpcao != "5");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
