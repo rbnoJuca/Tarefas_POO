ae68d0b8435f5ffe4085bc3abdebca2f4a4261c0
rbnoJuca <rodolfo.bergamo@gmail.com>
2020-09-16 17:14:02 -0300

    aula 07-09

---
+++ Aula 07-09/Exec_01/Exec_01/Retangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace Exec_01
+{
+    class Retangulo
+    {
+        public double v_dLargura;
+        public double v_dAltura;
+
+        public void Area(double Area)
+        {
+            Area = v_dAltura * v_dLargura;
+        }
+
+        public void Perimetro(double Per)
+        {
+            Per = 2 * (v_dAltura + v_dLargura);
+        }
+
+        public void Diagonal(double Dia)
+        {
+            Dia = Math.Sqrt(((v_dAltura * v_dAltura) + (v_dLargura * v_dLargura)));
+        }
+
+    }
+}
