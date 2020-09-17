b539ee4852806100dc7726a0a238a4a1b6f302a3
rbnoJuca <rodolfo.bergamo@gmail.com>
2020-09-17 15:51:43 -0300

    15h51min

---
+++ Aula 07-09/Exerc_03/Exerc_03/Exerc_03/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Exerc_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.WriteLine("Informe o aluno: ");
+            aluno.nome = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("Primeira nota - pontuação máxima: 30.\nSegunda nota - pontuação máxima: 35.\nTerceira nota - pontuação máxima: 35.");
+            Console.WriteLine("");
+            do
+            {
+                Console.WriteLine("Informe a primeira nota do aluno: ");
+                aluno.nota_1 = double.Parse(Console.ReadLine());
+            }
+            while (aluno.nota_1 < 0 || aluno.nota_1 > 30);
+
+            do
+            {
+                Console.WriteLine("Informe a segunda nota do aluno: ");
+                aluno.nota_2 = double.Parse(Console.ReadLine());
+            }
+            while (aluno.nota_2 < 0 || aluno.nota_2 > 35);
+
+            do
+            {
+                Console.WriteLine("informe a terceira nota do aluno: ");
+                aluno.nota_3 = double.Parse(Console.ReadLine());
+            }
+            while (aluno.nota_3 < 0 || aluno.nota_3 > 35);
+
+
+            if (aluno.SomaNota() >= 60)
+            {
+                Console.WriteLine("Aluno Aprovado.\nPontuação do aluno: " + aluno.SomaNota() + " pontos.");
+            }
+            else
+            {
+                Console.WriteLine("Aluno Reprovado.\nFaltaram " + (100 - aluno.SomaNota()) + " pontos.");
+            }
+
+        }
+    }
+}
