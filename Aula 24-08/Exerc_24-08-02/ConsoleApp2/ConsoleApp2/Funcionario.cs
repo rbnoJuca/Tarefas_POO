e69a3061891a88f503af82ad89d51d2a21a04f97
Rodolfo <rodolfo.bergamo@gmail.com>
2020-09-21 09:51:35 -0300

    21-09 09h51min

---
+++ Aula 24-08/Exerc_24-08-02/ConsoleApp2/ConsoleApp2/Funcionario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Funcionario
+    {
+
+        public string Nome;
+        public double Salario;
+
+
+    }
+}
---
+++ Aula 24-08/Exerc_24-08-02/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            
+            Funcionario funcionarioA = new Funcionario();
+            Funcionario funcionarioB = new Funcionario();
+
+            Console.WriteLine("Infome o nome do Primeiro Funcionário: ");
+            funcionarioA.Nome = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("Informe o salário do Funcionário: ");
+            funcionarioA.Salario = double.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            Console.WriteLine("Informe o Nome do Segundo Funcionário:");
+            funcionarioB.Nome = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("Informe o salário do funcionário:");
+            funcionarioB.Salario = double.Parse(Console.ReadLine());
+
+            double media = (funcionarioA.Salario + funcionarioB.Salario) / 2;
+
+            Console.WriteLine("Média salárial dos funcionários: " + media);
+
+        }
+    }
+}
